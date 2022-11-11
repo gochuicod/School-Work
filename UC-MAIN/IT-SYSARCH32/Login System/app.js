7a568fe4a590fef4d93c51c934c85835e2357e11
gochuicod <gochuicodarelleq@gmail.com>
2022-11-11 20:43:55 +0800

    School Work: IT-SYSARCH32 | Login System Update

--- UC-MAIN/IT-SYSARCH32/Login System/app.js
+++ UC-MAIN/IT-SYSARCH32/Login System/app.js
@@ -1,47 +1,66 @@
 const express = require("express"), mysql = require("mysql"), bodyparser = require("body-parser"), path = require("path");
 
 let port = process.env.port || 8000, app = express()
+let config = {
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "Sysint",
+    charset: "utf8mb4",
+    multipleStatement: true
+}
+let db = mysql.createPool(config)
+
+let server = app.listen(port, () => require("dns").lookup(require("os").hostname(), (err,addr,fam) => console.log(`http://${addr}:${port}`)))
 
 app.use(express.static('./public',{index:'login.html'}))
 app.use(bodyparser.urlencoded({"extended":true}))
 app.use(bodyparser.json())
 
-let config = {
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "user_management",
-    charset: "utf8mb4",
-    multipleStatement: true
-}
+app.get("/homepage",(req,res) => res.sendFile(`${__dirname}/public/index.html`))
 
-let db = mysql.createPool(config)
-
-app.get("/main",(req,res) => res.sendFile(`${__dirname}/public/index.html`))
-
-app.get("/users",(req,res) => {
-    let sql = "select * from tblusers"
-    db.query(sql, (err,results,fields) => err ? res.status(500).json(err) : res.json(results))
-})
-
-app.post("/users",(req,res) => {
+app.post("/user",(req,res) => {
     let users = req.body;
-    let sql = `insert into tblusers (username,password) values ('${users.username}','${users.password}')`;
-    db.query(sql, (err,results,fields) => err ? res.status(500).json(err) : res.json({"message":"New user added!"}))
+    let sql = `insert into tbluser (username,password) values ('${users.username}','${users.password}')`;
+    db.query(sql, err => err ? res.status(500).json(err) : res.json({"message":"New user added!"}))
 })
 
-app.delete("/users/:userID",(req,res) => {
-    let userID = req.params.userID;
-    let sql = `delete from tblusers where =${userID}`
-    db.query(sql, (err,results,fields) => err ? res.status(500).json(err) : res.json({"message":"User removed!"}));
+app.get("/user",(req,res) => {
+    let sql = "select * from tbluser"
+    db.query(sql, (err,results) => err ? res.status(500).json(err) : res.json(results))
 })
 
-app.put("/users",(req,res) => {
+app.put("/user",(req,res) => {
     let users = req.body;
-    let sql = `update tblusers set username='${users.username}', password='${users.password}' where userID=${users.userID}`;
-    db.query(sql, (err,results,fields) => err ? res.status(500).json(err) : res.json({"message":"User updated!"}));
+    let sql = `update tbluser set username='${users.username}', password='${users.password}' where id=${users.id}`;
+    db.query(sql, err => err ? res.status(500).json(err) : res.json({"message":"User updated!"}));
 })
 
-let server = app.listen(port, () => {
-    require("dns").lookup(require("os").hostname(), (err,addr,fam) => console.log(`http://${addr}:${port}`))
+app.delete("/user/:id",(req,res) => {
+    let sql = `delete from tbluser where id=${req.params.id}`
+    db.query(sql, err => err ? res.status(500).json(err) : res.json({"message":"User removed!"}));
+    // db.query(`alter table tbluser auto_increment=1`)
+})
+
+app.post("/student",(req,res) => {
+    let students = req.body;
+    let sql = `insert into tblstudent (lastname,firstname,course,level) values ('${students.lastname}','${students.firstname}','${students.course}','${students.level}')`;
+    db.query(sql, err => err ? res.status(500).json(err) : res.json({"message":"New studend added!"}))
+})
+
+app.get("/student",(req,res) => {
+    let sql = "select * from tblstudent"
+    db.query(sql, (err,results) => err ?  res.status(500).json(err) : res.json(results))
+})
+
+app.put("/student",(req,res) => {
+    let students = req.body;
+    let sql = `update tblstudent set lastname='${students.lastname}', firstname='${students.firstname}', course='${students.course}', level='${students.level}' where idno=${students.idno}`
+    db.query(sql, err => err ? res.status(500).json(err) : res.json({"message":"Student updated!"}))
+})
+
+app.delete("/student/:id",(req,res) => {
+    let sql = `delete from tblstudent where idno=${req.params.id}`
+    db.query(sql, err => err ? res.status(500).json(err) : res.json({"message":"Student removed!"}))
+    // db.query(`alter table tblstudent auto_increment=1`)
 })
--- UC-MAIN/IT-SYSARCH32/Login System/public/assets/js/index.js
+++ UC-MAIN/IT-SYSARCH32/Login System/public/assets/js/index.js
@@ -1,31 +1,41 @@
-(() => {
-    let app = angular.module("app",[]);
+let app = angular.module("app",[]);
 
-    app.controller("index",($scope,$http) => {
-        $scope.header=["username","password"];
+app.controller("index",($scope,$http) => {
+    $scope.header=["username","password"];
+    $http({
+        method:"GET",
+        url:"http://localhost:8000/user"
+    }).then(response => {
+        $scope.users=response.data
+    });
+})
+
+app.controller("register",($scope,$http) => {
+    $scope.register = () => {
         $http({
-            method:"GET",
-            url:"http://localhost:8000/users"
-        }).then(response => {
-            $scope.users=response.data
-        });
-    })
+            method:"POST",
+            url:"/user",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: {
+                username: $scope.username,
+                password: $scope.password
+            }
+        }).then(() => location.reload())
+    }
+})
 
-    app.controller("register",($scope,$http) => {
-        $scope.register = () => {
-            $http({
-                method:"POST",
-                url:"/users",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                data: {
-                    username: $scope.username,
-                    password: $scope.password
-                }
-            }).then(() => location.reload())
-        }
-    })
-})()
+let login = {
+    username: document.querySelector(".username"),
+    password: document.querySelector(".password"),
+    api: "http://localhost:8000/user",
+    compare: async function(){
+        const sample = await fetch(this.api)
+        const data = await sample.json()
+        if(this.username.value == "admin" && this.password.value == "user") window.location.href = "http://localhost:8000/homepage"
+        data.forEach(item => { if(this.username.value == item.username && this.password.value == item.password) window.location.href = "http://localhost:8000/homepage" })
+    }
+}
 
 let logout = () => window.location.href = "http://localhost:8000"
