b4cd7d2fe58462a1399418ee001603fdec87cbae
gochuicod <gochuicodarelleq@gmail.com>
2022-12-03 22:12:30 +0800

    School Work: IT-SYSARCH32 | Login System CRUD - updated and fixed bugs

--- UC-MAIN/IT-SYSARCH32/Login System/app.js
+++ UC-MAIN/IT-SYSARCH32/Login System/app.js
@@ -1,4 +1,4 @@
-const express = require('express'), mysql = require('mysql'), bodyparser = require('body-parser'), multer = require('multer'), fs = require('fs');
+const express = require('express'), mysql = require('mysql'), bodyparser = require('body-parser'), fileUpload = require('express-fileupload');
 
 let port = process.env.port || 8000, app = express()
 
@@ -13,74 +13,82 @@ let config = {
 }
 let db = mysql.createPool(config)
 
-// This section checks if desired image directory exists, otherwise creates one
-fs.access("./public/assets/images", error => {
-    error ? fs.mkdirSync('./public/assets/images', { recursive: true }) : console.log("Directory exists.")
-})
-
-// This section handles file data
-const fileStorageEngine = multer.diskStorage({
-    destination: (req,file,cb) => cb(null,'./public/assets/images'),
-    filename: (req,file,cb) => cb(null, `${Date.now()}--${file.originalname}`)
-})
-const upload = multer({storage:fileStorageEngine})
-
 app.listen(port, () => require("dns").lookup(require("os").hostname(), (err,addr,fam) => console.log(`http://${addr}:${port}`)))
 
 app.use(express.static(`${__dirname}/public`))
-app.use(bodyparser.urlencoded({"extended":true}))
+app.use(bodyparser.urlencoded({extended:true}))
 app.use(bodyparser.json())
+app.use(fileUpload({
+    createParentPath: true
+}))
 
+// This section is for the login page
 app.get("/",(req,res) => res.render("index.html"))
 
+// This section is for the main page
 app.get("/main",(req,res) => res.sendFile(`${__dirname}/public/main.html`))
 
+// This section creates a user
 app.post("/user",(req,res) => {
     let { username, password } = req.body;
     let sql = `insert into tbluser (username,password) values ('${username}','${password}')`;
     db.query(sql, err => err ? res.status(500).json(err) : res.redirect("/main"))
 })
 
+// This section selects all users
 app.get("/user",(req,res) => {
     let sql = "select * from tbluser"
     db.query(sql, (err,results) => err ? res.status(500).json(err) : res.json(results))
 })
 
-app.put("/user",(req,res) => {
+// This section updates a user's data
+app.put("/user/update",(req,res) => {
     let { username, password, id } = req.body;
     let sql = `update tbluser set username='${username}', password='${password}' where id=${id}`;
     db.query(sql, err => err ? res.status(500).json(err) : res.json({"message":"User updated!"}));
 })
 
+// This section deletes user data via id
 app.delete("/user/:id",(req,res) => {
     let sql = `delete from tbluser where id=${req.params.id}`
     db.query(sql, err => err ? res.status(500).json(err) : res.json({"message":"User removed!"}));
     db.query('alter table tbluser auto_increment=1')
 })
 
-app.post("/student",upload.single('image'),(req,res) => {
-    let { lastname, firstname, course, level } = req.body, { path } = req.file;
-    let sql = `insert into tblstudent (lastname,firstname,course,level,imagepath) values ('${lastname}','${firstname}','${course}','${level}','${path.slice(7,path.length)}')`;
-    db.query(sql, err => err ? res.status(500).json(err) : res.redirect("/main"))
+// This section creates student data
+app.post("/student", (req,res) => {
+    if(req.files){
+        let { lastname, firstname, course, level } = req.query, { webcam } = req.files;
+        let stamp = `${Date.now()}${webcam.name}`
+
+        webcam.mv(`./public/assets/images/${stamp}`)
+
+        let sql = `insert into tblstudent (lastname,firstname,course,level,imagepath) values ('${lastname}','${firstname}','${course}','${level}','./assets/images/${stamp}')`;
+        db.query(sql, err => err ? res.status(500).json(err) : res.redirect("/main"))
+    }
 })
 
+// This section selects all students
 app.get("/student",(req,res) => {
     let sql = "select * from tblstudent"
     db.query(sql, (err,results) => err ?  res.status(500).json(err) : res.json(results))
 })
 
-app.put("/student",(req,res) => {
+// This section updates a student's data
+app.put("/student/update",(req,res) => {
     let { idno, lastname, firstname, course, level } = req.body;
     let sql = `update tblstudent set lastname='${lastname}', firstname='${firstname}', course='${course}', level='${level}' where idno=${idno}`
     db.query(sql, err => err ? res.status(500).json(err) : res.json({"message":"Student updated!"}))
 })
 
+// This section deletes student data via id
 app.delete("/student/:id",(req,res) => {
     let sql = `delete from tblstudent where idno=${req.params.id}`
     db.query(sql, err => err ? res.status(500).json(err) : res.json({"message":"Student removed!"}))
     db.query('alter table tblstudent auto_increment=1')
 })
 
+// This section handles logins
 app.post("/login",(req,res) => {
     let { username, password } = req.body;
     let sql = `select * from tbluser where username='${username}' and password='${password}'`;
--- UC-MAIN/IT-SYSARCH32/Login System/public/assets/js/main.js
+++ UC-MAIN/IT-SYSARCH32/Login System/public/assets/js/main.js
@@ -1,37 +1,48 @@
-let app = angular.module("app",[]), toggle = false;
-let btnToggleVideo = document.querySelector(".btnToggleVideo"), messageprompt = document.querySelector(".messageprompt");
-let yesprompt = document.querySelector(".yesprompt"), noprompt = document.querySelector(".noprompt");
-let camera = document.querySelector(".camera");
+let app = angular.module("app",[]);
 
 app.run($rootScope => {
     $rootScope.levels = ["1","2","3","4"];
     $rootScope.courses = ["BSIT","BSCS","BSCPE","BSAMT"]
+    $rootScope.studentIdNo = 0;
+    $rootScope.studentImgPath = '';
+    $rootScope.userId = 0;
+    $rootScope.username = '';
+    $rootScope.password = '';
 })
 
-app.controller("userList",($scope,$http) => {
-    $scope.header=["id","username","password","edit | delete"];
+app.controller("userList",($scope,$http,$rootScope) => {
+    $scope.header=["username","password","edit | delete"];
     $http.get("/user").then(response => {
         $scope.users=response.data;
         if(response.data.length === 0) $scope.setDisplay = {"display":"none"}
         else $scope.setDisplay = {"display":"block"}
     });
     $scope.remove = id => $http.delete(`/user/${id}`).then(() => location.reload())
+    $scope.updateUser = (id,username,password) => {
+        $rootScope.userId = id;
+        $rootScope.username = username;
+        $rootScope.password = password;
+    }
 })
 
-app.controller("studentList",($scope,$http) => {
-    $scope.header=["idno","Image Profile","lastname","firstname","course","level","edit | delete"];
+app.controller("studentList",($scope,$http,$rootScope) => {
+    $scope.header=["lastname","firstname","course","level","edit | delete"];
     $http.get("/student").then(response => {
         $scope.students=response.data;
         if(response.data.length === 0) $scope.setDisplay = {"display":"none"}
         else $scope.setDisplay = {"display":"block"}
     });
     $scope.remove = idno => $http.delete(`/student/${idno}`).then(() => location.reload())
+    $scope.updateStudent = (idno,imagepath) => {
+        $rootScope.studentIdNo = idno;
+        $rootScope.studentImgPath = imagepath;
+    }
 })
 
-app.controller("updateUser",($scope,$http) => {
+app.controller("updateUser",($scope,$http,$rootScope) => {
     $scope.updateUser = () => {
-        $http.put("/user", {
-            id: $scope.id,
+        $http.put("/user/update", {
+            id: $rootScope.userId,
             username: $scope.username,
             password: $scope.password
         }).then(() => location.reload())
@@ -41,9 +52,12 @@ app.controller("updateUser",($scope,$http) => {
 app.controller("updateStudent",($scope,$http,$rootScope) => {
     $scope.courses = $rootScope.courses;
     $scope.levels = $rootScope.levels;
+    $rootScope.$watch('studentImgPath',() => {
+        $scope.image = $rootScope.studentImgPath;
+    })
     $scope.updateStudent = () => {
-        $http.put("/student",{
-            idno: $scope.idno,
+        $http.put("/student/update",{
+            idno: $rootScope.studentIdNo,
             lastname: $scope.lastname,
             firstname: $scope.firstname,
             course: $scope.selectedCourse,
@@ -52,64 +66,41 @@ app.controller("updateStudent",($scope,$http,$rootScope) => {
     }
 })
 
-const show = element => element.style.display = "block";
-const hide = element => element.style.display = "none";
-
-hide(messageprompt)
-hide(yesprompt)
-hide(noprompt)
+let lastname = document.querySelector(".lastname"), firstname = document.querySelector(".firstname");
+let course = document.querySelector(".course"), level = document.querySelector(".level");
+let video = document.querySelector(".video");
 
 Webcam.set({
-    width: 765,
-    height: 480,
+    width: 550,
+    height: 310,
     dest_width: 1080,
     dest_height: 720,
     image_format: 'jpeg',
     jpeg_quality: 100,
-    flip_horiz: true
+    flip_horiz: true,
 })
 
-btnToggleVideo.addEventListener("click", () => {
-    if(btnToggleVideo.children[0].classList.contains("bi-camera-video-off-fill")){
-        btnToggleVideo.children[0].setAttribute("class","bi bi-camera-video-fill")
+document.querySelector(".capture").addEventListener("click", () => Webcam.freeze())
+document.querySelector(".reset").addEventListener("click", () => Webcam.unfreeze())
+document.querySelector(".upload").addEventListener("click", () => {
+    if(lastname.value != "" || firstname.value != ""){
+        Webcam.snap(data_uri => {
+            Webcam.upload(data_uri,`/student?lastname=${lastname.value}&firstname=${firstname.value}&course=${course.value}&level=${level.value}`,(code,image) => {
+                alert("New Student Added!")
+                location.reload();
+            })
+        })
+    } else alert('Fill in the empty fields!')
+})
+
+video.addEventListener("click", () => {
+    if(video.firstChild.classList.contains("bi-camera-video-off-fill")){
+        video.setAttribute("class","video btn btn-dark")
+        video.firstChild.setAttribute("class","bi bi-camera-video-fill")
         Webcam.attach('.camera')
-        show(camera)
     } else {
-        btnToggleVideo.children[0].setAttribute("class","bi bi-camera-video-off-fill")
-        Webcam.reset('.camera');
-        hide(messageprompt)
-        hide(yesprompt)
-        hide(noprompt)
-        hide(camera)
+        video.setAttribute("class","video btn btn-danger")
+        video.firstChild.setAttribute("class","bi bi-camera-video-off-fill")
+        Webcam.reset('.camera')
     }
-})
-
-document.querySelector(".btnClose").addEventListener("click", () => {
-    btnToggleVideo.children[0].setAttribute("class","bi bi-camera-video-off-fill")
-    Webcam.reset('.camera')
-    hide(messageprompt)
-    hide(yesprompt)
-    hide(noprompt)
-    hide(camera)
-})
-
-document.querySelector(".btnSnapshot").addEventListener("click", () => {
-    Webcam.freeze();
-    show(messageprompt)
-    show(yesprompt)
-    show(noprompt)
-})
-
-yesprompt.addEventListener("click", () => {
-    hide(messageprompt)
-    hide(yesprompt)
-    hide(noprompt)
-    Webcam.snap(data_uri => download(data_uri))
-})
-
-noprompt.addEventListener("click", () => {
-    hide(messageprompt)
-    hide(yesprompt)
-    hide(noprompt)
-    Webcam.unfreeze();
 })
